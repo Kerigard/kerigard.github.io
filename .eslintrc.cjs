2378201e6fbb7a1bfa1d964b90ae687517f1fb1b
Kerigard <2fenix7@gmail.com>
2022-12-19 20:12:47 +0300

    build: change vue-cli to vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,13 @@
 module.exports = {
   root: true,
 
-  env: { node: true },
+  env: { browser: true, node: true },
 
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
   ],
 
@@ -23,7 +22,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
 
     'vue/require-default-prop': 'off',
   },
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{vue,js}'],
+  content: ['./index.html', './src/**/*.{vue,js}'],
 
   theme: {
     extend: {
